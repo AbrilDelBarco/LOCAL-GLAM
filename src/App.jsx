cdcd17bf20c11a0306982fd8f3fa272322c46717
abril del barco <delbarcoabrilm@gmail.com>
2023-02-15 21:53:59 -0300

    nuevasuvida

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navglam from './components/menu/Navbar';
+import Compcontainer from './components/compContainer/compcontainer';
+
+let main='soy el titulo de main'
+
+function App() {
+
+  return (
+    <>
+    <Navglam />
+
+    <Compcontainer />
+    </>
+  )
+}
+
+export default App
